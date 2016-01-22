b295b93a78a2dc79828b81a2de12f7d5aef4c14b
Danny Tunon <danieltunon@gmail.com>
2016-01-22 15:27:56 -0500

    Re-implement /uniq/

--- src/underbar.js
+++ src/underbar.js
@@ -97,6 +97,13 @@
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var uniqItems = [];
+    _.each(array, function( item ) {
+      if ( _.indexOf( uniqItems, item ) === -1 ) {
+        uniqItems.push( item );
+      }
+    });
+    return uniqItems;
   };
 
 
